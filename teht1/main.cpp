61b5479850b3efb46bafbb2f85d47d25c689fa1b
Antti <anttilempea@windowslive.com>
2025-01-08 13:24:44 +0200

    teht 1 valmis

---
+++ teht1/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int game(int);
+
+int main()
+{
+    int ylaraja;
+    cout << "anna ylaraja" << endl;
+    cin >> ylaraja;
+   int arvausten_maara = game(ylaraja);
+   cout << "arvasit " << arvausten_maara << " kertaa" << endl;
+
+    return 0;
+}
+
+
+int game(int maxnum){
+    int arvaus;
+    int arvausten_maara = 0;
+    srand(time(0));
+    int random = rand() % maxnum + 1;
+
+    do {
+        cout << "arvottu " << random << endl;
+        cout << "arvaa luku " << endl;
+        cin >> arvaus;
+        arvausten_maara++;
+
+        if (arvaus < random){
+            cout << "liian pieni" << endl;
+
+        }
+        else if(arvaus > random){
+            cout << "liian suuri" << endl;
+        }
+        else{
+            cout << "oikein!" << endl;
+
+        }
+
+    }
+    while(arvaus != random);
+    return arvausten_maara;
+}
